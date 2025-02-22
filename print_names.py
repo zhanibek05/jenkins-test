95dca8fc02ff94911de38c35040cd80afb30f98b
zhanibek05 <beisenov.zhanibek@bk.ru>
2025-02-22 10:21:39 +0500

    names from command

--- print_names.py
+++ print_names.py
@@ -1,3 +1,13 @@
-print("Beisenov Zhanibek")
-print("Dautbaev Danyar")
-print("Rakhmankul Bekzhan")
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <your string>")
+        return
+    
+    input_string = " ".join(sys.argv[1:])
+    print(input_string)
+
+if __name__ == "__main__":
+    main()
+
